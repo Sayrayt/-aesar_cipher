6119c688558c3e43da2dfb85f1b289eb80ec4123
paradox <101551548+Sayrayt@users.noreply.github.com>
2024-12-20 21:49:26 +0400

    Update router.jsx

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -8,6 +8,8 @@ import App from "../App.jsx";
 import Lab1 from "../pages/Lab1/Lab1.jsx";
 import Lab2 from "../pages/Lab2/Lab2.jsx";
 import Lab3_hash from "../pages/Lab3/Lab3_hash.jsx";
+import Lab4 from "../pages/Lab4/Lab4.jsx";
+import Lab5 from "../pages/Lab5/Lab5.jsx";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -15,6 +17,8 @@ const router = createBrowserRouter(
             <Route index element={<Lab1 />} />
             <Route path="lab2" element={<Lab2 />} />
             <Route path="lab3" element={<Lab3_hash />} />
+            <Route path="lab4" element={<Lab4 />} />
+            <Route path="lab5" element={<Lab5 />} />
         </Route>
     )
 );
